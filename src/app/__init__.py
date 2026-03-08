6cf99e592183793aa995c4f03d53751fe50561a0
micha003 <michael.vorogushyn@wbgym.de>
2026-03-08 10:48:00 +0100

    finished server config

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,11 +1,30 @@
 from flask import Flask
+from flask import render_template, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+from dotenv import load_dotenv
+import secrets
+
+db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__)
 
-    from .views import views
+    # TODO: Load environment variables from .env file
+    app.config["SECRET_KEY"] = secrets.token_hex(32)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///kwgost.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    app.register_blueprint(views)
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+    with app.app_context():
+        from .views import views
+
+        app.register_blueprint(views)
+
+        db.create_all()
 
     return app
