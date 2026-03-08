e685a95688aa11346381fd358d923bca50a9e17d
micha003 <michael.vorogushyn@wbgym.de>
2026-03-08 10:48:11 +0100

    added v1 of models

---
+++ src/app/models.py
@@ -0,0 +1,19 @@
+# DB Models
+
+from . import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    logincode = db.Column(db.String(80), unique=True, nullable=False)
+    first_name = db.Column(db.String(80), unique=True, nullable=False)
+    last_name = db.Column(db.String(80), unique=True, nullable=False)
+    grade = db.Column(db.Integer, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+
+class selection(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    selection_json = db.Column(db.Text, nullable=False)
+    finalized = db.Column(db.Boolean, default=False, nullable=False)
